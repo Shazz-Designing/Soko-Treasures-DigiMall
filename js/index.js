c37e89f1ed03b2669bd4701e44b03e56fad9b4c2
sharon <sapot@safaricom.co.ke>
2023-10-25 21:49:34 +0300

    Update and style the products for sale

--- js/index.js
+++ js/index.js
@@ -2,25 +2,21 @@
 fetch('./data/products.json')
   .then(response => response.json())
   .then(data => {
-    
-    
     // Display Available Products For Sale
+    const productGrid = document.getElementById('product-container');
 
-    const productContainer = document.getElementById('product-container');
-
-    // Assuming 'data' contains your JSON product data
     data.forEach(product => {
       const productElement = document.createElement('div');
+      productElement.classList.add('product-item'); // Add the product-item class
       productElement.innerHTML = `
         <h3>${product.name}</h3>
         <p>${product.description}</p>
         <p>Price: Ksh ${product.price.toFixed(2)}</p>
         <img src="${product.image}" alt="${product.name}">
       `;
-      productContainer.appendChild(productElement);
+      productGrid.appendChild(productElement);
     });
   })
-
   .catch(error => {
     console.error('Error loading JSON data: ', error);
   });
