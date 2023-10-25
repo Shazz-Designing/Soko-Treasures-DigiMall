6d4ec1470bb001c2d619731de662fedeb6ef8704
sharon <sapot@safaricom.co.ke>
2023-10-25 20:48:28 +0300

    Add code to fetch products from json file

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ fetch('products.json')
   .catch(error => {
     console.error('Error loading JSON data: ', error);
   });
+
+
+  // Display Available Products For Sale
+
+const productContainer = document.getElementById('product-container');
+
+// Assuming 'data' contains your JSON product data
+data.forEach(product => {
+  const productElement = document.createElement('div');
+  productElement.innerHTML = `
+    <h3>${product.name}</h3>
+    <p>${product.description}</p>
+    <p>Price: $${product.price.toFixed(2)}</p>
+    <img src="${product.image}" alt="${product.name}">
+  `;
+  productContainer.appendChild(productElement);
+});
