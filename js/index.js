2eb0e128c2d2da496b375240e28d9cd6d452a7ae
sharon <sapot@safaricom.co.ke>
2023-10-25 21:01:07 +0300

    Update js code as items are not being fetched

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 // Fetch the JSON data
-fetch('products.json')
+fetch('./data/products.json')
   .then(response => response.json())
   .then(data => {
+    
+    
+    // Display Available Products For Sale
+
+    const productContainer = document.getElementById('product-container');
+
+    // Assuming 'data' contains your JSON product data
+    data.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.innerHTML = `
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <p>Price: $${product.price.toFixed(2)}</p>
+        <img src="${product.image}" alt="${product.name}">
+      `;
+      productContainer.appendChild(productElement);
+    });
   })
+  
   .catch(error => {
     console.error('Error loading JSON data: ', error);
   });
-
-
-  // Display Available Products For Sale
-
-const productContainer = document.getElementById('product-container');
-
-// Assuming 'data' contains your JSON product data
-data.forEach(product => {
-  const productElement = document.createElement('div');
-  productElement.innerHTML = `
-    <h3>${product.name}</h3>
-    <p>${product.description}</p>
-    <p>Price: $${product.price.toFixed(2)}</p>
-    <img src="${product.image}" alt="${product.name}">
-  `;
-  productContainer.appendChild(productElement);
-});
